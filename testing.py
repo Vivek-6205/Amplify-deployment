1748c2c290745eec4c51893b03c305a05bc21061
practic_boy <practicboy121@gmail.com>
2025-09-05 08:11:46 +0530

    to add third line that explain auther checking

--- testing.py
+++ testing.py
@@ -1,2 +1,3 @@
 print("Hello donson kaise ho aap sabhi kya haal chaal")
 print("let's start the first programming in python and create the project")
+print("we are checking to change or not of auther name and email id that shit..")

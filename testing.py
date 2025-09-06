bdd6ee28b6fb25b2477fed05b83f546f8f13292b
practic_boy <practicboy121@gmail.com>
2025-09-06 08:01:59 +0530

    complited

--- testing.py
+++
@@ -1,3 +0,0 @@
-print("Hello donson kaise ho aap sabhi kya haal chaal")
-print("let's start the first programming in python and create the project")
-print("we are checking to change or not of auther name and email id that shit..")
